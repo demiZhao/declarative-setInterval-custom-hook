958fad62b239bbff010ff15e9a85fe4ad31545d4
demiZhao <demi.y.zhao@gmail.com>
2020-12-20 17:40:37 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+function Counter() {
+  const [delay, setDelay] = useState(1000);
+  const [count, setCount] = useState(0);
+
+  // Increment the counter.
+  useInterval(() => {
+    setCount(count + 1);
+  }, delay);
+
+  // Make it faster every second!
+  useInterval(() => {
+    if (delay > 10) {
+      setDelay(delay / 2);
+    }
+  }, 1000);
+
+  function handleReset() {
+    setDelay(1000);
+  }
+
+  return (
+    <>
+      <h1>Counter: {count}</h1>
+      <h4>Delay: {delay}</h4>
+      <button onClick={handleReset}>
+        Reset delay
+      </button>
+    </>
+  );
+}
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Counter />, rootElement);
